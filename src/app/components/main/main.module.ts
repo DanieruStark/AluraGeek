4c733111b2f4abaedd684dbb62592da22928df90
Daniel Santana <danielmaster19@gmail.com>
2022-09-04 07:34:32 -0300

    finalizada a parte star wars mobile

--- src/app/components/main/main.module.ts
+++ src/app/components/main/main.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { StarWarsComponent } from './components/star-wars/star-wars.component';
 
 
 
 @NgModule({
   declarations: [
     MainComponent,
-    BannerComponent
+    BannerComponent,
+    StarWarsComponent
   ],
   imports: [
     CommonModule
